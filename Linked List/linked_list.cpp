9a573b224fa14b5c4578ab7930cd39ad935d7615
Lucas <lucasrrt@gmail.com>
2017-03-17 00:05:40 -0300

    linked list

--- Linked List/linked_list.cpp
+++ Linked List/linked_list.cpp
@@ -1,3 +1,105 @@
-int main(){
+#include <iostream>
+#include <stdlib.h>
 
+using namespace std;
+
+struct node {
+	int value;
+	node *next;
+};
+
+void insertEnd(int value, node *&root){
+	node *current = root;
+	node *new_node = new node;
+	new_node->value = value;
+	new_node->next = NULL;
+	if(current == NULL)
+		root = new_node;
+	else{
+		while(current->next != NULL)
+			current = current->next;
+		current->next = new_node;
+	}
+}
+
+void insertBegin(int value, node* &root){
+	node *new_node = new node;
+	new_node->value = value;
+	new_node->next = root;
+	root = new_node;
+}
+
+void removeFirst(node *&root){
+	node* current = root;
+	if(current != NULL){
+		root = current->next;
+		free(current);
+	}
+}
+
+void removeValue(int value, node *&root){
+	node *current = root;
+	while(current->value == value){
+		removeFirst(root);
+		current = root;
+	}
+	node *previous = current;
+	current = current->next;
+	while(current != NULL){
+		if(current->value == value){
+			previous->next = current->next;
+			free(current);
+			current = previous->next;
+		}
+		else{
+			previous = current;
+			current = current->next;
+		}
+	}
+}
+
+void printList(node *root){
+	node* current = root;
+	while(current != NULL){
+		cout << current->value << "\n";
+		current = current->next;
+	}
+}
+
+int main(){
+	node *first = new node;
+	first->value = 24;
+	first->next = NULL;
+	insertBegin(10, first);
+	insertBegin(1,first);
+	insertBegin(1,first);
+	insertEnd(24,first);
+	insertEnd(24,first);
+	insertEnd(2,first);
+	insertEnd(1,first);
+	insertEnd(3,first);
+	insertEnd(4,first);
+
+	cout << "first print: " << "\n";
+	printList(first);
+
+	removeValue(1,first);
+	removeValue(24,first);
+	
+	cout << "second print: " << "\n";
+	printList(first);
+
+	removeFirst(first);
+	removeFirst(first);
+	removeFirst(first);
+	removeFirst(first);
+	
+	cout << "third print: " << "\n";
+	printList(first);
+	
+	insertEnd(24,first);
+	insertBegin(23,first);
+	
+	cout << "fourth print: " << "\n";
+	printList(first);
 }
